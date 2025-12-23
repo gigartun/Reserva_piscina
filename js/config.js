6f76f282f565014bdd624b1668a2bf63f5218812
gigartun <55601341+gigartun@users.noreply.github.com>
2025-12-23 13:53:40 -0500

    Update Firebase configuration and horarios

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 const firebaseConfig = {
     apiKey: "TU_API_KEY",
     authDomain: "TU_AUTH_DOMAIN",
-    projectId: "TU_PROJECT_ID",
+    projectId: "reserva-de-piscina",
     storageBucket: "TU_STORAGE_BUCKET",
     messagingSenderId: "TU_MESSAGING_SENDER_ID",
     appId: "TU_APP_ID"
@@ -18,7 +18,6 @@ const ADMIN_PIN = '1234';
 
 // Constantes
 const HORARIOS = [
-    "07:00-08:50",
     "09:00-10:50",
     "11:00-12:50",
     "15:00-16:50",
@@ -30,4 +29,5 @@ const HORARIO_SOLO_ADULTOS = "19:00-20:50";
 const MAX_PERSONAS_TURNO = 30;
 const MAX_INVITADOS = 2;
 const PRECIO_PROPIETARIO = 4;
+
 const PRECIO_INVITADO = 5;
